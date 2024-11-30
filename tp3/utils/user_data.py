9a51e3e212d4c67c72007374f15d6b0c6fe3fefa
Zabiullah-sz <zabiullah.shair-zaie@polymtl.ca>
2024-11-30 04:14:49 -0500

    manager and worker connected fully automated

--- tp3/utils/user_data.py
+++ tp3/utils/user_data.py
@@ -15,9 +15,9 @@ def get_manager_user_data():
     sudo systemctl restart mysql
     sudo systemctl enable mysql
 
-    # Set MySQL root password and create user
+    # Set MySQL root password and configure user
     sudo mysql -e 'ALTER USER "root"@"localhost" IDENTIFIED WITH mysql_native_password BY "password123";'
-    sudo mysql -u root -ppassword123 -e 'CREATE USER IF NOT EXISTS "root"@"%" IDENTIFIED BY "password123";'
+    sudo mysql -u root -ppassword123 -e 'CREATE USER IF NOT EXISTS "root"@"%" IDENTIFIED WITH mysql_native_password BY "password123";'
     sudo mysql -u root -ppassword123 -e 'GRANT ALL PRIVILEGES ON *.* TO "root"@"%";'
     sudo mysql -u root -ppassword123 -e 'FLUSH PRIVILEGES;'
 
@@ -43,6 +43,7 @@ def get_manager_user_data():
 
 
 
+
 def get_worker_user_data(manager_ip, server_id):
     return f"""#!/bin/bash
     exec > /var/log/worker_setup.log 2>&1
@@ -59,8 +60,20 @@ def get_worker_user_data(manager_ip, server_id):
     sudo systemctl restart mysql
     sudo systemctl enable mysql
 
-    # Set MySQL root password and configure replication
+    # Set MySQL root password
     sudo mysql -e 'ALTER USER "root"@"localhost" IDENTIFIED WITH mysql_native_password BY "password123";'
+    sudo mysql -e 'CREATE USER IF NOT EXISTS "root"@"%" IDENTIFIED WITH mysql_native_password BY "password123";'
+    sudo mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "root"@"%";'
+    sudo mysql -e 'FLUSH PRIVILEGES;'
+
+    # Install Sakila database
+    wget https://downloads.mysql.com/docs/sakila-db.tar.gz -P /tmp/
+    tar -xzvf /tmp/sakila-db.tar.gz -C /tmp/
+    sudo mysql -u root -ppassword123 -e 'CREATE DATABASE IF NOT EXISTS sakila;'
+    sudo mysql -u root -ppassword123 sakila < /tmp/sakila-db/sakila-schema.sql
+    sudo mysql -u root -ppassword123 sakila < /tmp/sakila-db/sakila-data.sql
+
+    # Configure replication
     sudo mysql -u root -ppassword123 -e 'STOP REPLICA;'
     sudo mysql -u root -ppassword123 -e "CHANGE REPLICATION SOURCE TO \
         SOURCE_HOST='{manager_ip}', \
@@ -68,25 +81,6 @@ def get_worker_user_data(manager_ip, server_id):
         SOURCE_PASSWORD='password123', \
         SOURCE_PORT=3306;"
     sudo mysql -u root -ppassword123 -e 'START REPLICA;'
-
-    # Install Sakila database for benchmarking consistency
-    wget https://downloads.mysql.com/docs/sakila-db.tar.gz -P /tmp/
-    tar -xzvf /tmp/sakila-db.tar.gz -C /tmp/
-    sudo mysql -u root -ppassword123 -e 'CREATE DATABASE IF NOT EXISTS sakila;'
-    sudo mysql -u root -ppassword123 sakila < /tmp/sakila-db/sakila-schema.sql
-    sudo mysql -u root -ppassword123 sakila < /tmp/sakila-db/sakila-data.sql
-
-    # Run Sysbench benchmark
-    sudo sysbench /usr/share/sysbench/oltp_read_only.lua \
-        --mysql-db=sakila \
-        --mysql-user="root" \
-        --mysql-password="password123" prepare
-
-    sudo sysbench /usr/share/sysbench/oltp_read_only.lua \
-        --mysql-db=sakila \
-        --mysql-user="root" \
-        --mysql-password="password123" run \
-        > /var/log/sysbench_benchmark.log 2>&1
     """
 
 
